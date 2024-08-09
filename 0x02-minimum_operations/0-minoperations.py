4cc7f17bdd392e78c52e7bc1d9ddc7b35f38cb1f
israamaher <48337918+israamaher@users.noreply.github.com>
2024-08-09 03:35:52 +0300

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,20 +3,21 @@
 Solution to minOperations problem
 """
 
+
 def minOperations(n):
     """
     write a method that calculates the fewest number
     of operations needed to result in exactly n H characters
     """
-    if n <= 1: 
-        return 0 
-    
+    if n <= 1:
+        return 0
+
     operation = 0
     divisor = 2
 
-    while(n > 1):
+    while (n > 1):
         while (n % divisor == 0):
             operation += divisor
-            n //= divisor 
+            n //= divisor
         divisor += 1
     return operation
